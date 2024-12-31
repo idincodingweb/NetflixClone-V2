64cf1f9a95fda963fdcbd2896683473af597d05a
Idin Iskandar <167355867+idincodingweb@users.noreply.github.com>
2025-01-01 03:44:03 +0700

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`
+    Authorization: `Bearer ${import.meta.env.API_KEY}`
   }
 });
 
